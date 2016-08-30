3675fad3fc22464236eb53aa0bca7d45c59f2832
0xshone <0xshone@gmail.com>
2016-08-31 03:19:00 +0900

    データモデルとの連携

---
+++ public/scripts/tutorial_9.js
@@ -0,0 +1,16 @@
+// tutorial_9.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+      <h1>Comments</h1>
+      <CommentList data={this.props.data} />
+      <CommentForm />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  document.getElementById('content')
+);
